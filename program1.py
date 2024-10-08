fdc8038ed5819c816510b2e8591f5b4c80378de7
saliimkhan <salimkhan41111@gmail.com>
2024-10-08 12:38:13 +0530

    salim khan

--- program1.py
+++ program1.py
@@ -1,6 +1,33 @@
 class Solution:
    
     def getTotalIsles(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(r,c):
+            if r<0 or r>= rows or c<0 or  cols or grid[r][c]=='W':
+                return
+            
+            grid[r][c]='W'
+
+            dfs(r-1,c)
+            dfs(r+1,c)
+            dfs(r,c-1)
+            dfs(r,c+1)
+        
+        island_count = 0
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 'L':
+                    dfs(r,c)
+                    island_count +=1
+        return island_count
+        
+
     #    write your code here
+
                     
-        return 0
+        
--- program2.py
+++ program2.py
@@ -1,5 +1,29 @@
 def decode_message( s: str, p: str) -> bool:
+        if len(s) != len(p):
+                return False
+        pattern_to_str = {}
+        str_to_pattern = {}
 
-# write your code here
-  
-        return False
+        for char_p, char_s in zip(p,s):
+                if char_p in pattern_to_str:
+                        if pattern_to_str[char_p] != char_s:
+                                return False
+                        
+
+                else:
+                        pattern_to_str[char_p] = char_s
+        
+        if char_s in str_to_pattern:
+                if str_to_pattern[char_s] != char_p:
+                        return False
+        else:
+                str_to_pattern[char_s] = [char_p]
+        return True
+                
+                
+        
+
+
+
+
+
